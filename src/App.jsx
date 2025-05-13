f17e118d24c7518aa3e496eac96965e21d508b87
Eazywrlddd <163927038+Eazywrlddd@users.noreply.github.com>
2025-05-13 11:18:51 -0500

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-
+import { puppyList } from "./data.js";
+import { useState } from "react";
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  console.log("puppyList: ", puppies);
   return (
     <>
-      <div>
-        
+      <div className="App">
+        {
+        puppies.map((puppy) => {
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+        })
+        }
+         {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
